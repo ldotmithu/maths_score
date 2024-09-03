710d5003df59f9c96ecd5a175826f61f69840363
ldotmithu <ldotmithurshan222@gmail.com>
2024-09-04 00:14:30 +0530

    add

--- .history/src/mlProject/constants/__init___20240903232825.py
+++ .history/src/mlProject/constants/__init___20240903232825.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import yaml
 
-CONFIG_FILE_PATH=Path('config/config.yaml')
+CONFIG_FILE_PATH=Path('config/congig.yaml')
 PARAMAS_FILE_PATH=Path('params.yaml')
 SCHEMA_FILE_PATH=Path('schema.yaml')
